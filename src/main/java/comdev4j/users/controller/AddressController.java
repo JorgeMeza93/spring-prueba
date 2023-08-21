095847b298dbd6cc6a43e1ec5d8d736fc957953c
Jorge Meza Bello <jorberiver@gmail.com>
2023-08-20 23:30:43 -0600

    Crear dirección

--- src/main/java/comdev4j/users/controller/AddressController.java
+++ src/main/java/comdev4j/users/controller/AddressController.java
@@ -2,10 +2,15 @@ package comdev4j.users.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +19,20 @@ import comdev4j.users.services.AddressService;
 
 
 @RestController
-@RequestMapping("/users/{userId}/profile/{profileId}/addresses")
+@RequestMapping("/users/{userId}/profiles/{profileId}/addresses")
 public class AddressController {
-	
+	public Logger logger = LoggerFactory.getLogger(AddressController.class);
+	@Autowired
 	private AddressService addressService;
 	
 	@GetMapping
 	public ResponseEntity<List<Address>> findAddressesByProfileId(@PathVariable("userId") Integer userId, @PathVariable("profileId") Integer profileId){
+		logger.info("Visitando direcciones");
 		return new ResponseEntity<List<Address>>(addressService.findAddressesByProfileAndUserId(userId, profileId), HttpStatus.OK);
 	}
+	@PostMapping
+	public ResponseEntity<Address> create(@PathVariable("userId") Integer userId, @PathVariable("profileId") Integer profileId, @RequestBody Address address){
+		return new ResponseEntity<Address>(addressService.createAddress(userId, profileId, address), HttpStatus.CREATED);
+	}
+	
 }
--- src/main/java/comdev4j/users/services/AddressService.java
+++ src/main/java/comdev4j/users/services/AddressService.java
@@ -1,12 +1,17 @@
 package comdev4j.users.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import comdev4j.users.entities.Address;
+import comdev4j.users.entities.Profile;
 import comdev4j.users.repositories.AddressRepository;
+import comdev4j.users.repositories.ProfileRepository;
 
 
 @Service
@@ -14,10 +19,20 @@ public class AddressService {
 
 	@Autowired
 	private AddressRepository addressRepository;
+	@Autowired
+	private ProfileRepository profileRepository;
 
-	
 	public List<Address> findAddressesByProfileAndUserId(Integer userId, Integer profileId) {
 		return addressRepository.findByProfileId(userId, profileId);
 	}
-	
+	public Address createAddress(Integer userId, Integer profileId, Address address) {
+		Optional<Profile> result = profileRepository.findByUserIdAndProfileId(userId, profileId);
+		if(result.isPresent()) {
+			address.setProfile(result.get());
+			return addressRepository.save(address);
+		}
+		else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Usuario con id %d y perfil id %d no encontrado", userId, profileId));
+		}
+	} 
 }
