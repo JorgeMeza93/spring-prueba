73aee3f0d108ace7fd939cd1b7885fe259b25c43
Jorge Meza Bello <jorberiver@gmail.com>
2023-09-03 23:52:52 -0600

    propiedad de numero de hilos disponibles

--- src/main/java/comdev4j/users/controller/Controller.java
+++ src/main/java/comdev4j/users/controller/Controller.java
@@ -1,10 +1,14 @@
 package comdev4j.users.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/saludo")
 public class Controller {
-
+	@GetMapping
+	public String saludo() {
+		return "!Hello World!";
+	}
 }
